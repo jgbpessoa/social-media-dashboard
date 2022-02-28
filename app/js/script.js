284908d0eba871677da5df65d9c460618edcf473
jgbpessoa <jgbpessoa@gmail.com>
2022-02-28 12:41:02 -0600

    dark mode logic and design details

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,30 @@
-console.log("Hello");
+const btnDark = document.querySelector("#dark");
+const btnLight = document.querySelector("#light");
+const btns = document.getElementsByName("theme");
+const systemPref = window.matchMedia("(prefers-color-scheme: dark)");
+
+const setMode = function (mode) {
+  document.body.removeAttribute("class");
+  document.body.classList.add(`${mode}`);
+  localStorage.setItem("mode", `${mode}`);
+};
+
+const modeLocalStorage = () => localStorage.getItem("mode");
+const modeSystemPref = () => (systemPref.matches ? "dark" : "light");
+
+const updateMode = function () {
+  const mode = modeLocalStorage() || modeSystemPref();
+  mode === "dark" ? btnDark.click() : btnLight.click();
+};
+
+systemPref.addEventListener("change", (e) => {
+  e.matches ? btnDark.click() : btnLight.click();
+});
+
+for (let btn of btns) {
+  btn.addEventListener("click", function () {
+    setMode(btn.id);
+  });
+}
+
+updateMode();
